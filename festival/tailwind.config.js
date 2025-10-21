d8451a0fe854327a34a2cf52cdedf43f9b5699be
noop-03 <a01073510242@gmail.com>
2025-10-21 21:51:13 +0900

    Create tailwind.config.js

---
+++ festival/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",      // App Router
+    "./components/**/*.{ts,tsx,js,jsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
