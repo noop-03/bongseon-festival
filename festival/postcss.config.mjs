31e914fddd0b56ad3191453c480324ab4ea315af
noop-03 <a01073510242@gmail.com>
2025-10-21 21:47:30 +0900

    Update postcss.config.mjs

--- festival/postcss.config.mjs
+++ festival/postcss.config.mjs
@@ -1,5 +1,10 @@
+// postcss.config.mjs
+/** @type {import('postcss').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
