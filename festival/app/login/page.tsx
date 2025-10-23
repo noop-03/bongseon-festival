667ec54b4294e765bae14abe01defac5e8d456db
noop-03 <a01073510242@gmail.com>
2025-10-23 18:15:21 +0900

    Update page.tsx

--- festival/app/login/page.tsx
+++ festival/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getBrowserClient } from "@/lib/browser-client";
+import { getBrowserClient } from "@/lib/supabase/browser-client";
 
 export default function LoginPage() {
   const supabase = getBrowserClient();
